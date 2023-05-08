4fe1439e4f8d748f85edb8b6e51f95b0b0c655bc
Maksimal <maksimal86@mail.ru>
2023-05-09 02:11:06 +0300

    09.05.2023 timer.py небольшие измения

--- Mining/timer.py
+++ Mining/timer.py
@@ -6,8 +6,8 @@ async def timer(timetime):
     print(time.strftime("%X", time.localtime()), timetime)
     t=time.strftime("%X", time.localtime())
     await  asyncio.sleep(60)
-    if int(t.replace(':', '')) <= int(timetime.replace(':', ''))+200 \
-            and int(t.replace(':', '')) >= int(timetime.replace(':', ''))-200:
+    if int(t.replace(':', '')) <= int(timetime.replace(':', ''))+100 \
+            and int(t.replace(':', '')) >= int(timetime.replace(':', ''))-50:
         print('timer return True'+timetime, t, int(timetime.replace(':', '')), int(t.replace(':', '')),int(timetime.replace(':', ''))+100, int(timetime.replace(':', ''))-100)
         return True
     else:
