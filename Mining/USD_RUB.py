66c21f49f48c2e81f0f0c91e2adbb88211a34fae
Maksimal <maksimal86@mail.ru>
2023-05-09 02:06:22 +0300

    09.05.2023 USD_RUB исправлена ошибка с кодировкой

--- Mining/USD_RUB.py
+++ Mining/USD_RUB.py
@@ -5,9 +5,7 @@ from bs4 import BeautifulSoup
 def get_course():
     print('run get_course')
     url = 'https://quote.rbc.ru/ticker/59111'
-    page=requests.get(url).text.encode('cp1251', errors='ignore').decode('utf-8', errors='ignore')
-    with open('index.html', 'w') as file:
-        file.write(page)
+    page=requests.get(url).text.encode('latin1', errors='ignore').decode('utf-8', errors='ignore')
     soup = BeautifulSoup(page, 'lxml')
     cource = soup.find('span', class_='chart__info__sum')
     delta_cource = cource.next_sibling.next_sibling.text.strip().translate({ord(i):None for i in '()'}).replace(',','.')
