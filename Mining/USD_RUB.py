7beac8ddb0bf3b4780cbf73b279bf560ea994e9f
Maksimal <maksimal86@mail.ru>
2023-09-21 23:28:53 +0300

    РЕфакторинг

--- Mining/USD_RUB.py
+++ Mining/USD_RUB.py
@@ -2,13 +2,31 @@
 import requests, lxml
 from bs4 import BeautifulSoup
 
-def get_course():
-    print('run get_course')
-    url = 'https://quote.rbc.ru/ticker/59111'
-    page=requests.get(url).text.encode('latin1', errors='ignore').decode('utf-8', errors='ignore')
-    soup = BeautifulSoup(page, 'lxml')
-    cource = soup.find('span', class_='chart__info__sum')
-    delta_cource = cource.next_sibling.next_sibling.text.strip().translate({ord(i):None for i in '()'}).replace(',','.')
+
+def get_page():
+    return requests.get(get_url()).text.encode('latin1', errors='ignore').decode('utf-8', errors='ignore')
+
+
+def get_soup():
+    return BeautifulSoup(get_page(), 'lxml')
+
+
+def get_cource_usd_rub():
+    return get_soup().find('span', class_='chart__info__sum')
+
+
+def get_url():
+    return 'https://quote.rbc.ru/ticker/59111'
+
+
+def get_delta_cource():
+    return get_cource_usd_rub().next_sibling.next_sibling.text.strip().translate({ord(i):None for i in '()'}).replace(',','.')
+
+
+def main():
+    cource = get_cource_usd_rub()
+    delta_cource = get_delta_cource()
+    print(cource.text, delta_cource)
     return cource.text, delta_cource
 if __name__ == '__main__':
-    get_course()
+    main()
