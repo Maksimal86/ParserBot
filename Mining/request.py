f41ed962c63f2257e122111b7270d36b85991d61
Maksimal <maksimal86@mail.ru>
2024-09-24 00:56:07 +0300

    Устранен повторный вызов функций после неоднократных нажатий "старт"

--- Mining/request.py
+++ Mining/request.py
@@ -6,22 +6,23 @@ import mytoken, USD_RUB
 import logging
 import datetime
 from aiogram import Bot, Dispatcher, executor, types
-import What_to_mine,hashrateno, mineros, Hive, binance, armtek,\
+import What_to_mine, hashrateno, mineros, Hive, binance, armtek, \
     monitoring_price_changes_minings_coins, timer, monitoring_the_number_of_rings_rplant as rplant
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
 quantity_rigs = 4
 
 # создали хранилище памяти
 storage = MemoryStorage()
 logger = logging.getLogger(__name__)
-bot=Bot(mytoken.tokenbot)
+bot = Bot(mytoken.tokenbot)
 #  передали экземпляру класса диспетчера бота и хранилище
-dp=Dispatcher(bot,storage=storage)
+dp = Dispatcher(bot, storage=storage)
 
 
-@dp.message_handler(content_types=['text'] )
+@dp.message_handler(content_types=['text'])
 async def send_message(message, state):
     but = types.ReplyKeyboardMarkup(resize_keyboard=True)
     btn1 = types.KeyboardButton('хешрейт')
@@ -32,34 +33,40 @@ async def send_message(message, state):
     btn6 = types.KeyboardButton('курсы валют')
     but.add(btn1, btn2, btn3, btn4, btn5, btn6)
     global course_change_observer
+
     course_change_observer = False
     print(message.text.lower(), message)
     if message.text.lower() == 'хешрейт':
         hashrate = rplant.monitoring_of_mining()
-        await bot.send_message(message.from_user.id, 'мгновенный хешрейт - '+ hashrate[1]
-                                + '\n средний хешрейт - ' + hashrate[2], reply_markup=but)
+        await bot.send_message(message.from_user.id, 'мгновенный хешрейт - ' + hashrate[1]
+                               + '\n средний хешрейт - ' + hashrate[2], reply_markup=but)
     elif message.text.lower() == 'старт':
         await bot.send_message(message.from_user.id, "запуск...", reply_markup=but)
-        print('старт')
-        if not course_change_observer:
+        print('course_change_observer =', course_change_observer)
+
+        if not course_change_observer and counter() == 1:
             course_change_observer = True
             await asyncio.gather(monitoring_price_changes(message), monitoring_number_of_rigs(message),
-                             monitoring_of_armtek_delivery(message))
+                                 monitoring_of_armtek_delivery(message))
+        else:
+            print('еще раз нажали "старт" ')
     elif message.text.lower() == 'стоп':
         course_change_observer = False
     elif message.text.lower() == 'профит':
         for j in hashrateno.main_function():
-            await bot.send_message(message.from_user.id, str(j).translate({ord(i):None for i in "[]'" }))
+            await bot.send_message(message.from_user.id, str(j).translate({ord(i): None for i in "[]'"}))
         for i in What_to_mine.get_profit_of_coins():
             await bot.send_message(message.from_user.id, i)
     elif message.text.lower() == 'армтек':
-       await get_data_from_armtek_and_send_message(message)
+        await get_data_from_armtek_and_send_message(message)
     elif message.text.lower() == 'курсы валют':
         for i in binance.get_cource_from_binance():
             await bot.send_message(message.from_user.id, i)
         for i in monitoring_price_changes_minings_coins.getting_coin_attrbutes():
             await  bot.send_message(message.from_user.id, text=i)
         await bot.send_message(message.from_user.id, text=USD_RUB.main())
+
+
 
 
 async def get_message_about_rigs(message):
@@ -80,11 +87,11 @@ async def monitoring_number_of_rigs(message):
         if quantity_rigs_online < quantity_rigs:
             await asyncio.sleep(0.3)
             await bot.send_message(message.from_user.id, text='Rig offline, online rigs = ' + str(
-                                                                    quantity_rigs_online)+'\n' + hashrate_of_rigs )
+                quantity_rigs_online) + '\n' + hashrate_of_rigs)
             await asyncio.sleep(0.5)
             mineros.period = 30
             with open('log.txt', 'a') as log:
-                log.write( str(datetime.datetime.now()) + 'rig offline' +
+                log.write(str(datetime.datetime.now()) + 'rig offline' +
                           str(quantity_rigs_online) + '\n')
         elif quantity_rigs_online == quantity_rigs:
             mineros.period = 180
@@ -92,31 +99,47 @@ async def monitoring_number_of_rigs(message):
 
 
 async def monitoring_price_changes(message):
-    # try:
-        while course_change_observer:
-            print('binance run', course_change_observer)
-            await get_message_about_rigs(message)
-            for i in binance.get_cource_from_binance():
-                try:
-                    print(i,(i[2][:-1]), float(i[2][:-1]))
-                    if float(i[2][:-1])>5 or float(i[2][:-1])<-5:
-                        await bot.send_message(message.from_user.id, text="Изменение больше 5% "+str(i))
-                except:
-                    continue
-            for i in monitoring_price_changes_minings_coins.hashrateno_get_coin_price():
-                print(i)
-                if i[3] == 'N/':
-                    await bot.send_message(message.from_user.id, text="По измененияем нет данных ")
-                elif float(i[3])>15 or float(i[3])<-15:
-                        await bot.send_message(message.from_user.id, text="Изменение больше 15% " + str(i).
-                                               translate({ord(i): None for i in '()'})+'% за 24 часа')
-            cource_rub_usd = USD_RUB.main()
-            await bot.send_message(message.from_user.id, text=cource_rub_usd)
-            print(cource_rub_usd)
-            await asyncio.sleep(3600*3)
-    # except:
-    #     with open("log.txt", 'a') as log:
-    #         log.write('monitoring_price_changes '+str(datetime.datetime.now())+' '+str(sys.exc_info())+'\n')
+
+    while course_change_observer:
+        print('binance run', course_change_observer)
+
+        await get_message_about_rigs(message)
+        for i in binance.get_cource_from_binance():
+            try:
+                print(i, (i[2][:-1]), float(i[2][:-1]))
+                if float(i[2][:-1]) > 5 or float(i[2][:-1]) < -5:
+                    await bot.send_message(message.from_user.id, text="Изменение больше 5% " + str(i))
+            except:
+                continue
+        for i in monitoring_price_changes_minings_coins.hashrateno_get_coin_price():
+            print(i)
+            if i[3] == 'N/':
+                await bot.send_message(message.from_user.id, text="По изменениям нет данных ")
+            elif float(i[3]) > 15 or float(i[3]) < -15:
+                await bot.send_message(message.from_user.id, text="Изменение больше 15% " + str(i).
+                                       translate({ord(i): None for i in '()'}) + '% за 24 часа')
+        cource_rub_usd = USD_RUB.main()
+        await bot.send_message(message.from_user.id, text=cource_rub_usd)
+        print(cource_rub_usd)
+        await asyncio.sleep(3600 * 3)
+
+
+def count_calls():
+    '''Счетчик нажатий кнопки "старт"'''
+    counter = 0
+    def closure(print_result=False):
+        nonlocal counter
+        if print_result:
+            return counter
+        counter += 1
+        return counter
+    return closure
+
+
+counter = count_calls()
+# except:
+#     with open("log.txt", 'a') as log:
+#         log.write('monitoring_price_changes '+str(datetime.datetime.now())+' '+str(sys.exc_info())+'\n')
 
 
 async def get_data_from_armtek_and_send_message(message):
@@ -139,7 +162,6 @@ async def get_data_from_armtek_and_send_message(message):
 
 
 async def monitoring_of_armtek_delivery(message):
-
     time_start_1 = '20:40'
     time_start_2 = '23:30'
     time_start_3 = '08:45'
@@ -203,5 +225,3 @@ except:
         log.write('start_polling ' + str(datetime.datetime.now()) + ' нет соединения ' + str(sys.exc_info()) + '\n')
     time.sleep(2)
     executor.start_polling(dp, skip_updates=True)
-
-
