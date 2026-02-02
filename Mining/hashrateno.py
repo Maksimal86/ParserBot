b68bb3d4ee7e04d6d62e2c72d90aa9ebc034645c
Maksimal86 <maksimal86@mail.ru>
2026-02-02 23:39:14 +0300

    Начало исправления hashrateno

--- Mining/hashrateno.py
+++ Mining/hashrateno.py
@@ -1,27 +1,25 @@
 # -*- coding: utf-8 -*-
 import time, re
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import requests, lxml
+from Selenium_Driver import get_driver_selenium_chrome
 from bs4 import BeautifulSoup
-from Selenium_Driver import set_options_of_selenium
 
 
-class Browser():
+class Browser:
     def __init__(self):
-        self.options = set_options_of_selenium()
-        self.s = Service(executable_path=r'C:/chromedriver.exe')
-        self.driver = webdriver.Chrome(options=self.options, service=self.s)
+        self.driver = get_driver_selenium_chrome()
 
 
     def selenium_driver_close(self):
         self.driver.close()
 
+
     def selenium_driver_quit(self):
         self.driver.quit()
 
-    def get_BeautifulSoup(self, card_name):
+
+    def get_beautifulsoup(self, card_name):
         url = f'https://www.hashrate.no/gpus/{card_name}'
         self.driver.get(url)
         page_source = self.driver.page_source.encode('cp1251', errors='ignore').decode('cp1251')
@@ -30,14 +28,16 @@ class Browser():
         soup = BeautifulSoup(page_source,'lxml')
         return soup
 
+
     def exclude_nice_hash(self):
         time.sleep(1)
         self.driver.find_element(By.XPATH,
                                  '/html/body/div/div[2]/div[5]/div[2]/div[2]/div[3]/div[2]/form[2]/div[3]/div[2]/div/'
                                  'input[1]').click()
 
+
     def get_main_tag(self):
-        main_teg = self.get_BeautifulSoup('5700').find('ul', attrs={"id": "myUL"})
+        main_teg = self.get_beautifulsoup('5700').find('ul', attrs={"id": "myUL"})
         return main_teg
 
 
@@ -57,15 +57,18 @@ class Browser():
             list_of_coins_profit.append(teg.strip(('$')))
         return list_of_coins_profit
 
+
     def get_dict_with_data(self):
         dict_with_data = {}
         for i, j in zip(self.get_list_of_names_coins(), self.get_list_of_coins_profit()):
             dict_with_data[i] = j
         return dict_with_data
 
+
     def get_sorting_dict(self):
         sorted_dict = sorted(self.get_dict_with_data().items(), key=lambda x: x[1], reverse=True)
         return dict(sorted_dict)
+
 
     def converting_to_string_format(self, card):
         result = card + ' ' + str(self.get_sorting_dict())
@@ -76,7 +79,7 @@ def main_function():
     cards = ['1080','5600xt','5700']
     data_of_coins = Browser()
     for i in cards:
-        data_of_coins.get_BeautifulSoup(i)
+        data_of_coins.get_beautifulsoup(i)
         data_of_coins.exclude_nice_hash()
         yield data_of_coins.converting_to_string_format(i)
     data_of_coins.selenium_driver_close()
